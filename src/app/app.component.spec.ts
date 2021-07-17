b7b7780c2fc5c48de554a3f7af228eaa758c1066
Sushma <priyankasushma13@gmail.com>
2021-07-17 19:22:31 +0530

    Flipkart project Added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Flipkart'`, () => {
+  it(`should have as title 'add-to-cart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Flipkart');
+    expect(app.title).toEqual('add-to-cart');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Flipkart app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('add-to-cart app is running!');
   });
 });
